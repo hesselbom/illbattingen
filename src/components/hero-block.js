906add01a184409c7b2f04e7c307a4dbff4fe961
Viktor Hesselbom <viktor@hesselbom.net>
2018-06-19 09:58:11 +0200

    Fix js issue with hero block on other pages

--- src/components/hero-block.js
+++ src/components/hero-block.js
@@ -1,13 +1,15 @@
 const image = document.querySelector('.hero-block > .image')
 
-const update = () => {
-  window.requestAnimationFrame(update)
+if (image) {
+  const update = () => {
+    window.requestAnimationFrame(update)
 
-  const doc = document.documentElement
-  const top = (window.pageYOffset || doc.scrollTop) - (doc.clientTop || 0)
-  const offset = (top / 10) * 0.5
+    const doc = document.documentElement
+    const top = (window.pageYOffset || doc.scrollTop) - (doc.clientTop || 0)
+    const offset = (top / 10) * 0.5
 
-  image.style.transform = `translate3d(0, ${offset}rem, 0)`
+    image.style.transform = `translate3d(0, ${offset}rem, 0)`
+  }
+
+  update()
 }
-
-update()
