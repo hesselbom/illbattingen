d5a0749fcb8da095cc2be57f0a6bccc43e57ca22
Viktor Hesselbom <viktor@hesselbom.net>
2018-06-21 09:28:45 +0200

    Add cta button

--- helpers/markdown.js
+++ helpers/markdown.js
@@ -1,7 +1,8 @@
 const md = require('markdown-it')({
   linkify: true,
   typographer: true,
-  breaks: true
+  breaks: true,
+  html: true
 })
 
 md
@@ -22,10 +23,26 @@ md.renderer.rules.table_open = function (tokens, idx, options, env, self) {
   return self.renderToken(tokens, idx, options)
 }
 
-module.exports = (text) => (
-  md.render(text)
+const extractProps = (str) => {
+  let props = {}
+  let arr
+  let regex = /([^=]*)="([^"]*)"/g
+  while (arr = regex.exec(str)) { // eslint-disable-line
+    props[arr[1].trim()] = arr[2]
+  }
+  return props
+}
+
+module.exports = (text) => {
+  // Fix buttons
+  text = text.replace(/\[button([^\]]*)\]/g, (_, p1) => {
+    let {text, url} = extractProps(p1)
+    return `<a href="${url}" class="text-button">${text}</a>`
+  })
+
+  return md.render(text)
     // Fix image paragraphs
     .replace(/<p>(<img[^>]*>)<\/p>/g, (_, p1) => (
       `<p class="image">${p1}</p>`
     ))
-)
+}
